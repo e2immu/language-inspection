4735ec8876fd44894498a617cb3b3e3d7a137702
Bart Naudts <Bart.Naudts@codelaser.io>
2024-10-08 12:29:19 +0200

    check for equivalent boolean in @GetSet, and allow accessors into arrays

--- e2immu-inspection-api/src/main/java/org/e2immu/language/inspection/api/util/GetSetUtil.java
+++ e2immu-inspection-api/src/main/java/org/e2immu/language/inspection/api/util/GetSetUtil.java
@@ -22,12 +22,12 @@ public class GetSetUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(GetSetUtil.class);
 
     public static void createSyntheticFieldsCorrespondingToGetSetAnnotation(Runtime runtime, TypeInfo typeInfo) {
-        TypeInfo.Builder builder = typeInfo.builder();;
+        TypeInfo.Builder builder = typeInfo.builder();
         builder.methods().stream().filter(MethodInfo::isAbstract).forEach(mi -> {
             AnnotationExpression getSet = mi.annotations().stream()
                     .filter(ae -> ae.typeInfo().fullyQualifiedName().equals(GetSet.class.getCanonicalName()))
                     .findFirst().orElse(null);
-            if (getSet != null) {
+            if (getSet != null && !mi.isFactoryMethod() && !getSet.extractBoolean("equivalent")) {
                 String fieldName;
                 String proposed = getSet.extractString("value", "");
                 if (!proposed.isBlank()) {
@@ -38,12 +38,7 @@ public class GetSetUtil {
                 boolean exists = builder.fields().stream().anyMatch(f -> fieldName.equals(f.name()));
                 if (!exists) {
                     LOGGER.debug("Create synthetic field for {}, named {}", mi, fieldName);
-                    ParameterizedType type;
-                    if(mi.parameters().isEmpty()) {
-                        type =  mi.returnType();
-                    } else  {
-                        type = mi.parameters().get(0).parameterizedType();
-                    }
+                    ParameterizedType type = extractFieldType(mi);
                     FieldInfo syntheticField = runtime.newFieldInfo(fieldName, false, type, typeInfo);
                     syntheticField.builder().setSynthetic(true)
                             .addFieldModifier(runtime.fieldModifierPrivate())
@@ -53,5 +48,33 @@ public class GetSetUtil {
                 }
             }
         });
+    }
+
+    private static ParameterizedType extractFieldType(MethodInfo mi) {
+        ParameterizedType type;
+        if (mi.parameters().isEmpty()) {
+            // T getT()
+            return mi.returnType();
+        }
+        if (mi.parameters().size() == 1) {
+            if (mi.isVoid()) {
+                // void setT(T t)
+                return mi.parameters().get(0).parameterizedType();
+            }
+            if (mi.parameters().get(0).parameterizedType().isInt()) {
+                // T getT(int i)   INDEXED
+                int a = mi.returnType().arrays();
+                return mi.returnType().copyWithArrays(a + 1);
+            }
+            // Builder newBuilder(URI uri)
+            throw new UnsupportedOperationException();
+        }
+        if (mi.parameters().size() == 2 && mi.parameters().get(0).parameterizedType().isInt()) {
+            // void setObject(int i, Object o)  INDEXED
+            ParameterizedType p1 = mi.parameters().get(1).parameterizedType();
+            int a = p1.arrays();
+            return mi.returnType().copyWithArrays(a + 1);
+        }
+        throw new UnsupportedOperationException();
     }
 }
