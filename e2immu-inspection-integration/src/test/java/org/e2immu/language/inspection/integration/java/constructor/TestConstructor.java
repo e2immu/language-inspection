4a691b299eeed2565e6d005ad4bb5a4a5f420f23
Bart Naudts <Bart.Naudts@codelaser.io>
2024-10-18 11:00:28 +0200

    extra test

--- e2immu-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/constructor/TestConstructor.java
+++ e2immu-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/constructor/TestConstructor.java
@@ -7,6 +7,7 @@ import org.e2immu.language.cst.api.info.TypeInfo;
 import org.e2immu.language.cst.api.statement.ReturnStatement;
 import org.e2immu.language.inspection.integration.java.CommonTest;
 import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,15 +16,15 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT1 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.util.ArrayList;
             import java.util.List;
-
+            
             /**
              * Basic constructor calling.
              */
             public class Constructor_0 {
-
+            
                 public void test() {
                     List<String> strings = new ArrayList<>();
                     List<String> list2 = new ArrayList<>(strings);
@@ -40,21 +41,21 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT2 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.util.ArrayList;
             import java.util.List;
-
+            
             /**
              * Generics on a constructor. I have never even encountered this until now (20211223).
              */
             public class Constructor_1 {
-
+            
                 static class Parametrized {
                     <T> Parametrized(T t, List<T> list) {
                         list.add(t);
                     }
                 }
-
+            
                 public void test() {
                     Parametrized p = new <String>Parametrized("3", new ArrayList<>());
                 }
@@ -70,28 +71,28 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT3 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import org.junit.jupiter.api.Test;
-
+            
             import java.lang.reflect.ParameterizedType;
             import java.lang.reflect.Type;
             import java.util.List;
-
+            
             import static org.junit.jupiter.api.Assertions.assertEquals;
             import static org.junit.jupiter.api.Assertions.assertThrows;
-
+            
             /**
              * GSon anonymous constructor trick
              */
             public class Constructor_2 {
-
+            
                 @Test
                 public void test() {
                     Type type = new TypeToken<List<String>>() {
                     }.type;
                     assertEquals("java.util.List<java.lang.String>", type.toString());
                 }
-
+            
                 @Test
                 public void test2() {
                     assertThrows(RuntimeException.class, () -> {
@@ -99,14 +100,14 @@ public class TestConstructor extends CommonTest {
                         assertEquals("java.util.List<java.lang.String>", type.toString());
                     });
                 }
-
+            
                 static class TypeToken<T> {
                     final Type type;
-
+            
                     TypeToken() {
                         this.type = getSuperclassTypeParameter(getClass());
                     }
-
+            
                     static Type getSuperclassTypeParameter(Class<?> subclass) {
                         Type superclass = subclass.getGenericSuperclass();
                         if (superclass instanceof Class) {
@@ -127,16 +128,16 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT4 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.util.List;
             import java.util.Map;
             import java.util.stream.Collectors;
-
+            
             public class Constructor_3 {
-
+            
                 static record MemberValuePair(String clazz, String s) {
                 }
-
+            
                 public void method(Map<Class<?>, Map<String, Object>> map) {
                     for (Map.Entry<Class<?>, Map<String, Object>> entry : map.entrySet()) {
                         List<Object> list;
@@ -161,30 +162,30 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT5 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.util.ArrayList;
             import java.util.List;
             import java.util.Map;
-
+            
             public class Constructor_4 {
-
+            
                 interface AnnotationExpression {
-
+            
                 }
-
+            
                 static class AnnotationExpressionImpl implements AnnotationExpression {
                     AnnotationExpressionImpl(String s) {
-
+            
                     }
                 }
-
+            
                 static class E2 {
-
+            
                     public String immutableAnnotation(Class<?> key, List<AnnotationExpression> list) {
                         return key.getCanonicalName();
                     }
                 }
-
+            
                 public void method(E2 e2) {
                     List<AnnotationExpression> list = new ArrayList<>();
                     Map<Class<?>, String> map = Map.of(String.class, "String");
@@ -204,20 +205,20 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT6 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.util.List;
-
+            
             /*
             Test because the if(b) statements messes up the variable contexts.
              */
             public class Constructor_5 {
-
+            
                 interface X {
                     String get();
-
+            
                     void accept(int j);
                 }
-
+            
                 // do NOT change the code of this method lightly, severely inspected in the test
                 public void method(List<X> xs) {
                     boolean b = xs.isEmpty();
@@ -228,7 +229,7 @@ public class TestConstructor extends CommonTest {
                             public String get() {
                                 return "abc " + s.toLowerCase();
                             }
-
+            
                             @Override
                             public void accept(int j) {
                                 System.out.println(s.toUpperCase() + ";" + j);
@@ -247,12 +248,12 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT7 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.util.HashSet;
             import java.util.List;
-
+            
             public class Constructor_6 {
-
+            
                 public void method(List<String> strings) {
                     assert new HashSet<>(strings).size() > 1;
                 }
@@ -267,11 +268,11 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT8 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             public class Constructor_7 {
-
+            
                 record R(String... strings) {}
-
+            
                 public int method() {
                     R r1 = new R();
                     R r2 = new R("a");
@@ -289,11 +290,11 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT9 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             public class Constructor_8 {
-
+            
                 record Pair<K, V>(K k, V v) {}
-
+            
                 public int method() {
                    return new Pair<>("3", 3).k.length();
                 }
@@ -309,16 +310,16 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT10 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             public class Constructor_9 {
-
+            
                 interface Analyser {
                     record SharedState(int iteration, String context) {}
                 }
-
+            
                 static class ParameterAnalyser implements Analyser {
                     record SharedState(int iteration) {}
-
+            
                     public void method() {
                         SharedState sharedState = new SharedState(3);
                     }
@@ -334,34 +335,34 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT11 = """
             public class Constructor_10Scope {
-
+            
                 private final int i;
-
+            
                 public int getI() {
                     return i;
                 }
-
+            
                 public Constructor_10Scope(int i) {
                     this.i = i;
                 }
-
+            
                 public class Sub {
                     final int j;
-
+            
                     Sub(int j) {
                         this.j = j;
                     }
-
+            
                     @Override
                     public String toString() {
                         return "together = " + i + ", " + j;
                     }
                 }
-
+            
                 Sub getSub(int j) {
                     return this.new Sub(j);
                 }
-
+            
                 static Sub copy(Constructor_10Scope c) {
                     return c.new Sub(c.i);
                 }
@@ -381,24 +382,24 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT12 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.util.HashMap;
             import java.util.Map;
-
-
+            
+            
             public class Constructor_11CommonConstructorBlock {
-
+            
                 private final Map<String, Integer> map;
-
+            
                 {
                     map = new HashMap<String, Integer>();
                 }
-
+            
                 public Constructor_11CommonConstructorBlock(String s, String t) {
                     map.put(s, 1);
                     map.put(t, 2);
                 }
-
+            
                 public Integer get(String s) {
                     return map.get(s);
                 }
@@ -413,16 +414,16 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT13 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.util.HashMap;
             import java.util.Map;
-
+            
             public class Constructor_12DoubleBrace {
                 static Map<String, String> map = new HashMap<>() {{
                     put("1", "a");
                     put("2", "b");
                 }};
-
+            
             }
             """;
 
@@ -442,12 +443,12 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT14 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             public class Constructor_13A {
-
+            
                 class Inner {
                     int value;
-
+            
                     Inner() {
                         super();
                     }
@@ -463,15 +464,15 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT15 = """
             package org.e2immu.language.inspection.integration.java.importhelper;
-
+            
             import java.util.HashMap;
             import java.util.Map;
             
             class Constructor_13B {
-
+            
                 private Constructor_13A a;
                 private final Map<String, Object> map = new HashMap<>();
-
+            
                 void method() {
                     a.new Inner().value = 3;
                     map.put("key", a.new Inner());
@@ -488,18 +489,18 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT16 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.io.File;
             import java.util.Arrays;
             import java.util.List;
-
+            
             public class Constructor_14 {
-
-
+            
+            
                 static class TempFile {
                     TempFile(File file, boolean b) {
                     }
-
+            
                     private List<TempFile> toTempFiles(File[] files) {
                         // problem: the type produced by Arrays.asList(files).stream() is not good enough
                         // Arrays.stream(files)  == OK
@@ -519,11 +520,11 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT17 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.util.HashMap;
-
+            
             public class Constructor_15 {
-
+            
                 void test() {
                     new HashMap<>();
                 }
@@ -538,13 +539,13 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT18 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             public class Constructor_16 {
-
+            
                 public Constructor_16() {
                     System.out.println("This is the constructor");
                 }
-
+            
                 public void Constructor_16() {
                     System.out.println("bad practice, but legal!");
                 }
@@ -559,38 +560,38 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT19 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.util.Set;
-
+            
             public class Constructor_17 {
-
+            
                 static class G<T> {
-
+            
                 }
-
+            
                 static class V<T> {
-
+            
                 }
-
+            
                 static class BreakCycles<T> {
                     public BreakCycles(ActionComputer<T> actionComputer) {
-
+            
                     }
                 }
-
+            
                 interface ActionComputer<T> {
                     Action<T> compute(G<T> g, Set<V<T>> cycle);
                 }
-
+            
                 interface Action<T> {
                     G<T> apply();
                     ActionInfo<T> info();
                 }
-
+            
                 public interface ActionInfo<T> {
-
+            
                 }
-
+            
                 void method(V<String> v6, V<String> v9) {
                     BreakCycles<String> bc2 = new BreakCycles<>((g1, cycle) -> {
                         if (cycle.contains(v6)) {
@@ -599,7 +600,7 @@ public class TestConstructor extends CommonTest {
                                 public G<String> apply() {
                                     return g1;
                                 }
-
+            
                                 @Override
                                 public ActionInfo<String> info() {
                                     return null;
@@ -623,20 +624,20 @@ public class TestConstructor extends CommonTest {
     @Language("java")
     private static final String INPUT20 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             public class Constructor_18 {
-
+            
                 interface I {
-
+            
                 }
                 I i;
-    
+            
                 Constructor_18(String s, I... is) {
                 }
-
+            
                 Constructor_18(Object... objects) {
                 }
-
+            
                 static Constructor_18 method(String s) {
                     return new Constructor_18(s);
                 }
@@ -646,5 +647,37 @@ public class TestConstructor extends CommonTest {
     @Test
     public void test20() {
         javaInspector.parse(INPUT20);
+    }
+
+
+    @Language("java")
+    public static final String INPUT21 = """
+            package a.b;
+            public class X {
+                record Pair<F, G>(F f, G g) {
+                }
+                record R<F, G>(Pair<F, G> pair) {
+                    public R {
+                        assert pair != null;
+                    }
+                }
+                static <X, Y> R<Y, X> method(R<X, Y> r) {
+                    return new R<>(new Pair<>(r.pair.g, r.pair.f));
+                }
+            }
+            """;
+
+    @DisplayName("parameter-less record constructor")
+    @Test
+    public void test21() {
+        TypeInfo X = javaInspector.parse(INPUT21);
+        TypeInfo R = X.findSubType("R");
+        assertEquals(2, R.constructors().size());
+
+        MethodInfo cR = R.findConstructor(1);
+        assertFalse(cR.isCompactConstructor());
+
+        MethodInfo cc = R.findConstructor(0);
+        assertTrue(cc.isCompactConstructor());
     }
 }
