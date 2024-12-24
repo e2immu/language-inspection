3fc05aae0a4309022aacb6554a136b35790980e4
Bart Naudts <Bart.Naudts@codelaser.io>
2024-12-24 10:20:43 +0100

    improved EvalInstanceOf, with test in language-inspection

--- e2immu-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/other/TestOperators.java
+++ e2immu-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/other/TestOperators.java
@@ -1,15 +1,16 @@
 package org.e2immu.language.inspection.integration.java.other;
 
 import org.e2immu.language.cst.api.expression.BinaryOperator;
+import org.e2immu.language.cst.api.expression.Expression;
 import org.e2immu.language.cst.api.info.MethodInfo;
 import org.e2immu.language.cst.api.info.TypeInfo;
+import org.e2immu.language.cst.api.output.element.Symbol;
 import org.e2immu.language.cst.api.statement.IfElseStatement;
 import org.e2immu.language.inspection.integration.java.CommonTest;
 import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestOperators extends CommonTest {
     /*
@@ -18,13 +19,13 @@ public class TestOperators extends CommonTest {
     @Language("java")
     private static final String INPUT1 = """
             package org.e2immu.analyser.resolver.testexample;
-
+            
             import java.util.HashMap;
             import java.util.Map;
-
+            
             public class Basics_9 {
                 private final Map<String, Integer> map = new HashMap<>();
-
+            
                 public int method1(String k) {
                     Integer v = map.get(k);
                     int r;
@@ -48,5 +49,61 @@ public class TestOperators extends CommonTest {
             && ifElse.expression() instanceof BinaryOperator bo) {
             assertSame(javaInspector.runtime().equalsOperatorObject(), bo.operator());
         } else fail();
+    }
+
+    @Language("java")
+    private static final String INPUT2 = """
+            package org.e2immu.analyser.resolver.testexample;
+            
+            import java.util.Collection;
+            import java.util.HashMap;
+            import java.util.Map;
+            
+            public class X {
+            
+                public int method1(Map<String, Integer> map) {
+                   assert map instanceof Map;
+                   assert map instanceof HashMap<String,Integer>;
+                   assert map instanceof Collection<?>; // interface -> still possible
+                   assert map instanceof Object;
+                   assert map instanceof Exception; // class -> not possible
+                }
+            }
+            """;
+
+    @Test
+    public void test2() {
+        TypeInfo typeInfo = javaInspector.parse(INPUT2);
+        MethodInfo method1 = typeInfo.findUniqueMethod("method1", 1);
+        assertTrue(eval(method1, 0).isBoolValueTrue());
+        assertEquals("map instanceof HashMap<String,Integer>", eval(method1, 1).toString());
+        assertEquals("map instanceof Collection<?>", eval(method1, 2).toString());
+        assertTrue(eval(method1, 3).isBoolValueTrue());
+        assertTrue(eval(method1, 4).isBoolValueFalse());
+    }
+
+    private Expression eval(MethodInfo method1, int i) {
+        return javaInspector.runtime().sortAndSimplify(method1.methodBody().statements().get(i).expression());
+    }
+
+    interface M {
+    }
+
+    interface C {
+    }
+
+    class HM implements M {
+    }
+
+    class HMS extends HM implements C {
+    }
+
+    M m = new HMS();
+
+    @Test
+    public void testJava() {
+        assertFalse(M.class.isAssignableFrom(C.class));
+        assertFalse(C.class.isAssignableFrom(M.class));
+        assertTrue(m instanceof C);
     }
 }
