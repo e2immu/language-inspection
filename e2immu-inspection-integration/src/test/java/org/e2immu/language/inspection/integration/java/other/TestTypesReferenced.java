3c6e8f932ff0a05cba6371a6039834bb8cdb1912
Bart Naudts <Bart.Naudts@codelaser.io>
2025-05-10 16:43:12 +0200

    extra test

---
+++ e2immu-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/other/TestTypesReferenced.java
@@ -0,0 +1,39 @@
+package org.e2immu.language.inspection.integration.java.other;
+
+import org.e2immu.language.cst.api.info.TypeInfo;
+import org.e2immu.language.inspection.integration.java.CommonTest;
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestTypesReferenced extends CommonTest {
+
+    @Language("java")
+    public static final String INPUT1 = """
+            package a.b;
+            import java.lang.annotation.Annotation;
+            class X {
+                interface Y<A extends Annotation> {
+                   A supply();
+                }
+            }
+            """;
+
+    @Test
+    public void test1() {
+        TypeInfo typeInfo = javaInspector.parse(INPUT1);
+        assertEquals("""
+                [TypeReference[typeInfo=java.lang.Object, explicit=true], \
+                TypeReference[typeInfo=java.lang.Object, explicit=true], \
+                TypeReference[typeInfo=java.lang.annotation.Annotation, explicit=true], \
+                TypeReference[typeInfo=java.lang.annotation.Annotation, explicit=true]]\
+                """, typeInfo.typesReferenced().toList().toString());
+        String expect = """
+                package a.b;
+                import java.lang.annotation.Annotation;
+                class X { interface Y<A extends Annotation> { A supply(); } }
+                """;
+        assertEquals(expect, javaInspector.print2(typeInfo));
+    }
+}
