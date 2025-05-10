0804f0fd709759c5e5b958a4b9dc538555bc6287
Bart Naudts <Bart.Naudts@codelaser.io>
2025-05-10 13:50:47 +0200

    extra test for non-erased exception types

--- e2immu-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/type/TestByteCode.java
+++ e2immu-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/type/TestByteCode.java
@@ -7,6 +7,7 @@ import org.e2immu.language.inspection.integration.java.CommonTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.setup.AbstractMockMvcBuilder;
 
+import java.io.FileOutputStream;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,11 +21,29 @@ public class TestByteCode extends CommonTest {
                 B extends org.springframework.test.web.servlet.setup.AbstractMockMvcBuilder<B>>]\
                 """, typeInfo.typeParameters().stream()
                 .map(TypeParameter::toStringWithTypeBounds).collect(Collectors.joining(", ")));
-        MethodInfo apply  = typeInfo.findUniqueMethod("apply", 1);
+        MethodInfo apply = typeInfo.findUniqueMethod("apply", 1);
         assertEquals("""
                 T=TP#0 in AbstractMockMvcBuilder.apply [Type param B extends \
                 org.springframework.test.web.servlet.setup.AbstractMockMvcBuilder<B>]\
                 """, apply.typeParameters().stream()
                 .map(TypeParameter::toStringWithTypeBounds).collect(Collectors.joining(", ")));
+    }
+
+    @Test
+    public void testThrows() {
+        TypeInfo typeInfo = javaInspector.compiledTypesManager().getOrLoad(FileOutputStream.class);
+        MethodInfo close = typeInfo.findUniqueMethod("close", 0);
+        assertEquals("java.io.FileOutputStream.close()", close.fullyQualifiedName());
+        assertEquals("Type java.io.IOException", close.exceptionTypes().getFirst().toString());
+    }
+
+    @Test
+    public void testThrows2() {
+        TypeInfo typeInfo = javaInspector.compiledTypesManager().getOrLoad("java.lang.ScopedValue");
+        TypeInfo carrier = typeInfo.findSubType("Carrier");
+        MethodInfo close = carrier.findUniqueMethod("call", 1);
+        assertEquals("java.lang.ScopedValue.Carrier.call(CallableOp<? extends R,X extends Throwable>)",
+                close.fullyQualifiedName());
+        assertEquals("Type param X extends Throwable", close.exceptionTypes().getFirst().toString());
     }
 }
