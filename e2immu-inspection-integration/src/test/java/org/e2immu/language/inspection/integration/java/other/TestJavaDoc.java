49a0dd4228783f6b0d7deb10e36049cfe143a8f1
Bart Naudts <Bart.Naudts@codelaser.io>
2025-06-10 08:23:34 +0200

    extra test

--- e2immu-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/other/TestJavaDoc.java
+++ e2immu-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/other/TestJavaDoc.java
@@ -1,11 +1,14 @@
 package org.e2immu.language.inspection.integration.java.other;
 
+import org.e2immu.language.cst.api.element.JavaDoc;
+import org.e2immu.language.cst.api.info.MethodInfo;
 import org.e2immu.language.cst.api.info.TypeInfo;
 import org.e2immu.language.inspection.integration.java.CommonTest;
 import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TestJavaDoc extends CommonTest {
     @Language("java")
@@ -29,6 +32,17 @@ public class TestJavaDoc extends CommonTest {
     @Test
     public void test1() {
         TypeInfo typeInfo = javaInspector.parse(INPUT1);
+        MethodInfo methodInfo = typeInfo.findUniqueMethod("method", 2);
+        JavaDoc javaDoc = methodInfo.javaDoc();
+        assertNotNull(javaDoc);
+        assertEquals(4, javaDoc.tags().size());
+        JavaDoc.Tag t0 = javaDoc.tags().getFirst();
+        assertEquals("a.b.X.method(String,int):0:in", t0.resolvedReference().toString());
+        assertEquals("5-15:5-16", t0.sourceOfReference().compact2());
+        JavaDoc.Tag t1 = javaDoc.tags().get(1);
+        assertEquals("a.b.X.method(String,int):1:len", t1.resolvedReference().toString());
+        assertEquals("6-15:6-17", t1.sourceOfReference().compact2());
+
         @Language("java")
         String expected = """
                 package a.b;
