8eeb2ae92e0da565bcf0475be44c685bc165612a
Bart Naudts <Bart.Naudts@codelaser.io>
2025-07-02 08:03:25 +0200

    extra test example for import/hierarchy/sub

---
+++ e2immu-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/example/Primary1.java
@@ -0,0 +1,15 @@
+package org.e2immu.language.inspection.integration.java.example;
+
+public class Primary1 {
+
+    static class Sub1 extends Primary2.Sub2 {
+
+    }
+    static class Sub {
+
+    }
+
+    static class SubSubSub extends Primary2.SubSub {
+
+    }
+}
---
+++ e2immu-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/example/Primary2.java
@@ -0,0 +1,11 @@
+package org.e2immu.language.inspection.integration.java.example;
+
+public class Primary2 extends Primary1 {
+
+    static class Sub2 {
+
+    }
+    static class SubSub extends Sub {
+
+    }
+}
